6a0c50b5afd87344c8f7e3b3b0e5f7274461cd49
Hamza Imrnist <hamza.imranist@gmail.com>
2020-02-29 20:56:25 +0500

    faux painting home page

---
+++ js/app.js
@@ -0,0 +1,46 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("selector");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+showSlides1();
+function showSlides1() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides1, 4000); // Change image every 2 seconds
+}
+let menuButton = document.querySelector(".menu-button");
+let mobileNav = document.querySelector(".nav-container-mobile");
+let mobileMenu = document.querySelector(".menu");
+$(function() {
+  $( ".menu-button" ).click(function() {
+      $( ".nav-container-mobile" ).toggle("slow");
+  });
+});
